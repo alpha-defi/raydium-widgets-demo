fc5496647b7d7746d380e6dd0561e72002c47342
alpha.defi.9@gmail.com <alpha.defi.9@gmail.com>
2022-06-02 01:28:52 -0700

    Add changes to publish to github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  PUBLIC_URL: "https://alphadefi.github.io/raydium-widgets-demo/",
+  assetPrefix: './'
 }
 
 module.exports = nextConfig
